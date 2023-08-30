7ff293484e59a7ef09dcda54a211b61b33c590c8
youngduklee <bd_lee@bigin.io>
2023-08-30 20:46:55 +0900

    feat: rest api 작성

--- src/index.js
+++ src/index.js
@@ -7,19 +7,55 @@ import jwt from "jsonwebtoken";
 
 const app = express();
 
+let users = [
+    {
+        id: 1,
+        name: "dugi",
+        age: 31,
+    }
+];
+
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: true, limit: "700mb"}))
 
-const today = new Date();
-const todayTodayjs = dayjs(today).format("YYYY.MM.DD");
-console.log({ today, todayTodayjs });
+app.get("/users", (req, res) => {
+    res.status(200).json({users});
+});
+app.post("/users", (req, res) => {
+    const {name, age} = req.body;
 
-const password = "1234";
-const hashedPassword = bcrypt.hashSync(password, 10);
-console.log({hashedPassword});
+    users.push({
+        id: new Date().getTime(),
+        name,
+        age,
+    });
 
-const token = jwt.sign("1234", "secret_key");
-console.log({token});
+    res.status(201).json({users});
+});
+app.patch("/users/:id", (req, res) => {
+    const {id} = req.params;
+    const {name, age} = req.body;
+
+    const targetUserIdx = users.findIndex((user) => user.id === Number(id));
+
+    users[targetUserIdx] = {
+        id: users[targetUserIdx].id,
+        name: name ?? users[targetUserIdx].name,
+        age: age ?? users[targetUserIdx].age,
+    }
+
+    res.status(204).json({});
+});
+app.delete("/users/:id", (req, res) => {
+    const {id} = req.params;
+
+    const deleteUsers = users.filter((user) => user.id !== Number(id));
+    users = deleteUsers;
+
+    res.status(204).json({});
+});
 
 app.get("/", (req, res) => {
     res.send("Nodejs 강의 재미있는데?")
